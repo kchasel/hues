04fad7beb1847a32d744752cab1ecf9fc8788e67
Kenneth Lay <kchasel@me.com>
2015-07-02 00:52:31 -0400

    Adds SunEvent, Sunrise, Sunset, Sun

--- src/main/scala/hues/Runner.scala
+++ src/main/scala/hues/Runner.scala
@@ -1,46 +1,7 @@
 package hues
 
-import scala.concurrent._
-import scala.concurrent.ExecutionContext.Implicits.global
-
 class Runner(latitude: Option[Double], longitude: Option[Double]) {
-  val solarClock = new SolarClock(latitude.getOrElse(40.0), longitude.getOrElse(-73.3))
-
-  val periodInDays = (45.0/60)/24
-
   def start = {
-    scheduleSunset()
+    new Sunrise(latitude.getOrElse(40.3), longitude.getOrElse(-73.3))
   }
-
-  val cal = java.util.Calendar.getInstance
-  cal.set(2015, 6, 1, 16, 59, 0)
-
-  def scheduleSunset(): Unit = {
-    (new java.util.Timer(true)).schedule(new java.util.TimerTask() {
-      override def run = {
-        scheduleTimer(5000) {
-          println("SETTING THE SUN")
-        }
-        scheduleSunrise()
-      }
-    }, cal.getTime)//SolarClock.julianToDate(solarClock.sunset - periodInDays))
-  }
-
-  def scheduleSunrise() = {
-    Thread.sleep(15000)
-    (new java.util.Timer(true)).schedule(new java.util.TimerTask() {
-      override def run: Unit = {
-        scheduleTimer(5000) {
-          println("RAISING THE SUN")
-        }
-        scheduleSunset()
-      }
-    }, SolarClock.julianToDate(solarClock.sunrise + periodInDays))
-  }
-
-
-  def scheduleTimer(period: Int)(timerFn: => Unit) =
-    (new java.util.Timer(true)).scheduleAtFixedRate(new java.util.TimerTask() {
-      override def run = timerFn
-    }, 0, period)
 }
---
+++ src/main/scala/hues/SunEvent.scala
@@ -0,0 +1,44 @@
+package hues
+
+import java.util.{Date, Timer, Calendar}
+
+abstract class SunEvent(lat: Double, lng: Double, on: Long = SolarClock.currentJulianDate()) {
+  val solarClock = new SolarClock(lat, lng, on)
+
+  val cal = java.util.Calendar.getInstance
+
+  cal.add(Calendar.MINUTE, 1)
+
+  def triggerTime: Double
+
+  println(s"ENQUEEING $this AT ${SolarClock.julianToDate(triggerTime)}")
+  Sun.timer.schedule(new java.util.TimerTask() {
+    override def run: Unit = {
+      trigger()
+    }
+  }, cal.getTime)
+  //}, SolarClock.julianToDate(triggerTime))
+
+  def trigger() = {
+    println(s"EVENT TRIGGERED: ${this}")
+    Sun.prepareNext(this)
+  }
+}
+
+object Sun {
+  val periodInDays = (45.0/60)/24
+  val timer = new Timer(true)
+
+  def prepareNext(existing: SunEvent): SunEvent = existing match {
+    case Sunrise(lat, lng, _) => Sunset(lat, lng)
+    case Sunset(lat, lng, _) => Sunrise(lat, lng)
+  }
+}
+
+case class Sunrise(lat: Double, lng: Double, on: Long = SolarClock.currentJulianDate()) extends SunEvent(lat, lng, on) {
+  lazy val triggerTime = solarClock.sunrise
+}
+
+case class Sunset(lat: Double, lng: Double, on: Long = SolarClock.currentJulianDate()) extends SunEvent(lat, lng, on) {
+  lazy val triggerTime = solarClock.sunset - Sun.periodInDays
+}
